c1700e933e640501d87feaa2c5d3fffa8aa18424
Andy Mockler <asmockler@gmail.com>
2014-10-02 20:34:50 -0600

    Menu

--- Fay/scripts/main.js
+++ Fay/scripts/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-	var menuItems = $('.menu ul');
-	menuItems.hide();
+	var menu = $('.menu ul');
+	var menuItems = menu.find('li');
 	$('.menu .fay-menu-float').on('click', function(){
-		if ( !$(this).attr('data-fay-open') ){
-			menuItems.fadeIn();
+		if ( $(this).attr('data-fay-open') == "true" ){
+			menuItems.each(function (index) {
+				$(this).delay(index*150).animate({
+					'right': 0
+				}, 900, 'easeOutExpo');
+			});
+		} else if ( $(this).attr('data-fay-open') == "false" ){
+			menuItems.each(function (index) {
+				$(this).delay(index*25).animate({
+					'right': -$(this).outerWidth()
+				}, 500, 'easeOutExpo');
+			});
 		}
 	});
 });
