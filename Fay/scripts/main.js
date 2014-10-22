d4852a252012b7af7d6430ab85ddc1e1a53ea5bb
asmockler <asmockler@gmail.com>
2014-10-22 13:44:39 -0600

    Fay Docs Pretty Much Finished

--- Fay/scripts/main.js
+++ Fay/scripts/main.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	var menu = $('.menu ul');
 	var menuItems = menu.find('li');
 	$('.menu .fay-menu-float').on('click', function(){
-		if ( $(this).attr('data-fay-open') == "true" ){
+		if ( $(this).attr('data-fay-open') === "false" ){
 			menuItems.each(function (index) {
 				$(this).delay(index*150).animate({
 					'right': 0
 				}, 900, 'easeOutExpo');
 			});
-		} else if ( $(this).attr('data-fay-open') == "false" ){
+		} else if ( $(this).attr('data-fay-open') === "true" ){
 			menuItems.each(function (index) {
 				$(this).delay(index*25).animate({
 					'right': -$(this).outerWidth()
@@ -16,4 +16,5 @@ $(document).ready(function(){
 			});
 		}
 	});
+	Fay.init();
 });
