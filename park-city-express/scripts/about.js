161a7d63462f68f3fd0d992865ccbb21998feb88
asmockler <asmockler@gmail.com>
2014-12-08 22:44:46 -0700

    Add Park City Express

---
+++ park-city-express/scripts/about.js
@@ -0,0 +1,43 @@
+$('#sidebar a').on('click', function(e){
+
+	if ($(this).hasClass('active')) {
+		return false
+	} else {
+		switch ($(this).html()) {
+			case 'About Us':
+				$('.visible').fadeOut(300, function(){
+					$('.visible').removeClass('visible');
+					$('.active').removeClass('active');
+					$('#about-us').addClass('visible').fadeIn(300);
+					$('#about-link').addClass('active');
+				});
+				break;
+			case 'Our Mission':
+				$('.visible').fadeOut(300, function(){
+					$('.visible').removeClass('visible');
+					$('.active').removeClass('active');
+					$('#mission').addClass('visible').fadeIn(300);
+					$('#mission-link').addClass('active');
+				});
+				break;
+			case 'Our Drivers':
+				$('.visible').fadeOut(300, function(){
+					$('.visible').removeClass('visible');
+					$('.active').removeClass('active');
+					$('#drivers').addClass('visible').fadeIn(300);
+					$('#drivers-link').addClass('active');
+				});
+				break;	
+			case 'Terms of Service':
+				$('.visible').fadeOut(300, function(){
+					$('.visible').removeClass('visible');
+					$('.active').removeClass('active');
+					$('#terms').addClass('visible').fadeIn(300);
+					$('#terms-link').addClass('active');
+				});
+				break;
+		}
+	}
+
+	
+});
---
+++ park-city-express/scripts/index.js
@@ -0,0 +1,30 @@
+var changeSubText = function () {
+	var $this = $('#sub-title span');
+	var currentWord = $this.html();
+	var word;
+
+	switch (currentWord) {
+		case '':
+			word = 'Park City resorts.'
+			break;
+		case 'Park City resorts.':
+			word = 'Sundance Film Festival.'
+			break;
+		case 'Sundance Film Festival.':
+			word = 'the best restaurants.'
+			break;
+		case 'the best restaurants.':
+			word = 'Park City resorts.'
+			break;
+	}
+
+	$this.fadeOut(200, function(){
+		$this.html(word).fadeIn(200);
+	});
+}
+
+changeSubText();
+
+window.setInterval(function(){
+  changeSubText();
+}, 2500);
