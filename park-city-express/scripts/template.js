bb8183c36d63d6b54c745c1bf6ed286077b516b4
asmockler <asmockler@gmail.com>
2014-12-16 18:54:49 -0700

    Lots o' Changes
    Make new pages visible in mobile dropdown; link to PCE Facebook; resize
    banner image to improve load time; add careers content; fix page
    titles; improve reserve now page; improve behavior of nabber on static
    pages; fix reserve now button margin

--- park-city-express/scripts/template.js
+++ park-city-express/scripts/template.js
@@ -1,7 +1,9 @@
 $(document).scroll(function(){
-	if ( $(this).scrollTop() > 300 ){
-		$('.navbar').css('background', 'rgba(255,255,255,.87)')
-	} else if ( $(this).scrollTop() < 300 ) {
-		$('.navbar').css('background', 'rgba(255,255,255,0')
+	if ( $(document).width() > 768 ) {
+		if ( $(this).scrollTop() > 100 ){
+			$('.navbar').css('background', 'rgba(255,255,255,.87)')
+		} else if ( $(this).scrollTop() < 100 ) {
+			$('.navbar').css('background', 'rgba(255,255,255,0')
+		}
 	}
 });
